3e8b3793ff719392900b6d74db9442fb8ed68a9c
Bitcidic <138175839+Bitcidic@users.noreply.github.com>
2023-10-26 23:48:10 -0400

    Delete VoxylSnake directory

--- VoxylSnake/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module voxylware.voxylsnake {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens voxylware.voxylsnake to javafx.fxml;
-    exports voxylware.voxylsnake;
-}
